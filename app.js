ec64f0de4febac1cffff92956140ccaa6e8b9c4e
chocoflames <ezevalentine107@gmail.com>
2022-07-28 14:34:11 +0100

    coding the javascript

---
+++ app.js
@@ -0,0 +1,11 @@
+let quoteText = document.querySelector(".quote");
+let quoteBtn = document.querySelector("button");
+
+// random quote function name
+
+function randomQuote() {
+   
+    });
+}
+
+quoteBtn.addEventListener("click", randomQuote)
