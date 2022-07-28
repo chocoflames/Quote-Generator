8fa8b617c5f2887bbfb76099765caa840536e05a
chocoflames <ezevalentine107@gmail.com>
2022-07-28 14:37:02 +0100

    calling a random quote api in javascript

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ let quoteBtn = document.querySelector("button");
 // random quote function name
 
 function randomQuote() {
-   
+  // getting random api/data and parsing it into javascript object
+  fetch("http://api.quotable.io/random")
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result);
     });
 }
 
-quoteBtn.addEventListener("click", randomQuote)
+quoteBtn.addEventListener("click", randomQuote);
