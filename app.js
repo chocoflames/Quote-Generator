82a7799cb937ba0869e98bbf05cb7fd4ef84ff06
chocoflames <ezevalentine107@gmail.com>
2022-07-28 17:35:54 +0100

    coding the copy text for the quote text

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let quoteText = document.querySelector(".quote");
 let authorName = document.querySelector(".name");
 let soundBtn = document.querySelector(".sound");
+let copyBtn = document.querySelector(".copy");
 let quoteBtn = document.querySelector("button");
 
 // random quote function name
@@ -22,8 +23,14 @@ function randomQuote() {
 
 soundBtn.addEventListener("click", () => {
   // SpeechSynthesisUtterance is a web speech api that represent a speech request
-  let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText}`) by (`${authorName.innerText}`);
+  let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
   speechSynthesis.speak(utterance) // speak method of speechSynthesis speak the utterance
+})
+
+copyBtn.addEventListener("click", () => {
+  // copying the quote text on copybtn click
+  // writeText() property writes the specified text string to the system clipboard
+  navigator.clipboard.writeText(quoteText.innerText)
 })
 
 quoteBtn.addEventListener("click", randomQuote);
