acff73b9c43e1cfee313c228282bb55f33081a4d
chocoflames <ezevalentine107@gmail.com>
2022-07-28 15:10:57 +0100

    creating a new classlist on my quote btn called add

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 let quoteText = document.querySelector(".quote");
+let authorName = document.querySelector(".name");
 let quoteBtn = document.querySelector("button");
 
 // random quote function name
 
 function randomQuote() {
+    quoteBtn.classList.add("loading")
+    quoteBtn.innerText = "Loading Quote..."
   // getting random api/data and parsing it into javascript object
   fetch("http://api.quotable.io/random")
     .then((res) => res.json())
     .then((result) => {
-      console.log(result);
+    //   console.log(result);
+    quoteText.innerText = result.content;
+    authorName.innerText = result.author;
+    quoteBtn.innerText = "New Quote"
     });
 }
 
